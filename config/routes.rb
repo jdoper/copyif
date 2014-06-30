6cd8ca6a3f685f6d3d3d7b576dbb8ca8aec6be2a
nunesdennis <DennisNunes@MacBook-Air-de-Dennis.local>
2014-06-30 13:02:56 -0300

    Mudanças no Layout
     mudanças feitas por Pedro de Layout.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'login/index'
+
   resources :fotocopia
 
   resources :usuarios
@@ -7,7 +9,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'login#index', as: 'login'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
