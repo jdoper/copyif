5b629551b6071192de8234d9916f44ae25ea2834
jdoper <atomas395@gmail.com>
2014-08-23 23:50:48 -0300

    Alterando tipo do campo Diretoria

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -2,4 +2,6 @@ class Usuario < ActiveRecord::Base
   validates :matricula, presence: true, uniqueness: true
   validates :tipo, presence: true
   has_many :fotocopium
+  DIRETORIAS = [ "DIATINF", "DIACON", "DIAREN" ]
+  USER_TYPES = [ "professor", "diretor", "grafica" ]
 end
