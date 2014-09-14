ca2ea8cb8c8cc54286276280bc1b8ea2c00d03f6
Dennis da Silva Nunes <nunesdennis@me.com>
2014-09-13 21:06:34 -0300

    Testes com heroku

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140908025902) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "admins", force: true do |t|
     t.integer  "senha"
     t.integer  "limiteCopias"
